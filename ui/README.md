a6a5045ce058eba1c97971ef3aead807e21835c1
Zezula Uros <Uros.Zezula@psi.ch>
2016-03-09 13:54:52 +0100

    [COSYLAB-532]: Add option for data buffer transmission mode

--- ui/README.md
+++ ui/README.md
@@ -10,8 +10,11 @@ Usage: $0 [options]
 Options:
 - -s <system name>     The system/project name
 - -d <EVR name>        Timing card name (default: EVR0)
+- -n <regDev name>     regDev name (default: EVR0DBUF)"
+- -m <signal mode>     For Data Buffer Transmission mode use 'Tx' (default: Rx)"
 - -h                   This help
 
+
 ## Files description
-- __G_TI_BUNCHID.ui__ Qt Panel that displays the data buffer readout of the EVR.
+- __G_TI_BUNCHID.ui__  Qt Panel that displays the data buffer readout of the EVR.
 
--- ui/start_TimDBuff.sh
+++ ui/start_TimDBuff.sh
@@ -2,12 +2,13 @@
 set -o errexit
 
 #Set caqtdm display path - to retrieve the stylesheet file
-export CAQTDM_DISPLAY_PATH=/sf/op/config/qt:/sf/rf/config/qt:$CAQTDM_DISPLAY_PATH
+export CAQTDM_DISPLAY_PATH=/sf/op/config/qt:/sf/rf/config/qt:/sf/controls/config/qt
 
 SYS=""
 DEVICE="EVR0"
 NAME="EVR0DBUF"
 SCREEN="G_TI_BUNCHID.ui"
+SIG="Rx"
 
 usage()
 {
@@ -16,12 +17,13 @@ usage()
     echo "    -s <system name>     (required) The system/project name"
     echo "    -d <DEVICE name>     Timing card name (default: $DEVICE)"
     echo "    -n <regDev name>     regDev name (default: $NAME)"
+    echo "    -m <signal mode>     For Data Buffer Transmission mode use 'Tx' (default: $SIG)"
     echo "    -h                   This help"
 }
 
 s_flag=0 # reset s_flag (-s is required)
 
-while getopts ":s:d:n:h" o; do
+while getopts ":s:d:n:m:h" o; do
     case "${o}" in
         s)
             SYS=${OPTARG}
@@ -33,6 +35,10 @@ while getopts ":s:d:n:h" o; do
 
         n)
             NAME=${OPTARG}
+            ;;
+
+        m)
+            SIG=${OPTARG}
             ;;
 
         h)
@@ -52,7 +58,7 @@ if [ $s_flag -eq 0 ]; then
     exit 1
 fi
 
-macro="SYS=$SYS,NAME=$NAME,DEVICE=$DEVICE"
+macro="SYS=$SYS,NAME=$NAME,DEVICE=$DEVICE,SIG=$SIG"
 
 #'startDM' should be replaced with 'caqtdm' once the new version of caqtdm is out.
 startDM -stylefile sfop.qss -macro "$macro" $SCREEN &
