0d5a9ae7afb84b163145c964beb95e5191494872
Roger Biffiger <roger.biffiger@psi.ch>
2020-03-25 10:09:42 +0100

    Added sending HW perf counter values via segmented data buffer

--- mrfioc2_regDev_pulseID_TX.cmd
+++ mrfioc2_regDev_pulseID_TX.cmd
@@ -7,5 +7,5 @@ require SynApps
 mrfioc2_regDevConfigure $(NAME=$(DEVICE=EVG0)DBUF$(TYPE=300)) $(DEVICE=EVG0) $(TYPE=300) $(PROTOCOL=0) $(USER_OFFSET=16)
 
 ## Data buffer receive template for pulse ID
-dbLoadRecords("$(mrfioc2_regDev_TEMPLATES)/pulseId_TX.template", "P=$(SYS)-$(DEVICE=EVG0):, NAME=$(NAME=$(DEVICE=EVG0)DBUF$(TYPE=300)), ID=$(ID=)")
+dbLoadRecords("$(mrfioc2_regDev_TEMPLATES)/pulseId_TX.template", "P=$(SYS)-$(DEVICE=EVG0):, NAME=$(NAME=$(DEVICE=EVG0)DBUF$(TYPE=300)), ID=$(ID=), SEQ-ID=$(SYS)-TMA:Perf.VALA, SEQ1-START-HW-CTR=$(SYS)-TMA:Perf.VALB, SEQ1-END-HW-CTR=$(SYS)-TMA:Perf.VALC, SEQ2-START-HW-CTR=$(SYS)-TMA:Perf.VALD, SEQ2-END-HW-CTR=$(SYS)-TMA:Perf.VALE")
 dbLoadRecords("$(mrfioc2_regDev_TEMPLATES)/perf.template", "P=$(SYS)-$(DEVICE=EVG0):, ID=$(ID=), MODE=TX, AUTORST_TIME=$(PERF_AUTORST_TIME=60)")
